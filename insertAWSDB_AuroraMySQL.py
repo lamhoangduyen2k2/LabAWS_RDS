2b006a72d03cca69ee3548a6e5b6bed484dfc9fb
lamhoangduyen2k2 <97307113+lamhoangduyen2k2@users.noreply.github.com>
2022-10-30 14:55:56 +0700

    first commit

---
+++ insertAWSDB_AuroraMySQL.py
@@ -0,0 +1,14 @@
+#Code này mình cũng có tham khảo của bạn Minh Sơn
+import insertUtil as ut
+import MySQLdb
+
+
+#Tạo kết nối với RDS Aurora MySQL và thêm dữ liệu vào
+conn = MySQLdb.connect(host='database-aurora.cluster-ctnk8ti4crcr.us-east-1.rds.amazonaws.com', user='admin', passwd='12345678', db='covid19_aurora', port=3306)
+print('Open DB successfully')
+ut.insert(conn)
+ut.select(conn)
+conn.close()
+print('Successfully')
+
+
---
+++ insertAWSDB_PostgresQL.py
@@ -0,0 +1,12 @@
+#Code này mình cũng có tham khảo của bạn Minh Sơn
+import insertUtil as ut
+import psycopg2
+
+
+#Tạo kết nối với RDS PostgreSQL và thêm dữ liệu vào
+conn = psycopg2.connect(database='covid19_postgres', user='postgres', password='12345678', host='database-postgresql.ctnk8ti4crcr.us-east-1.rds.amazonaws.com', port='5432')
+print('Open DB successfully')
+ut.insert(conn)
+ut.select(conn)
+conn.close()
+print('Successfully')
